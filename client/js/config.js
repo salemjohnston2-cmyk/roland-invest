0251d6698a28ad2480b6a413eec9564028f60916
Salem Uredo Ayegba <salemjohnston2@gmail.com>
2026-09-13 14:22:03 +0100

    Create client/js/config.js

---
+++ client/js/config.js
@@ -0,0 +1,4 @@
+// Single source of truth for the API base URL.
+// Dev:  http://localhost:4000
+// Prod: your Render URL, e.g. https://roland-invests-api.onrender.com
+window.API_BASE = "http://localhost:4000";
